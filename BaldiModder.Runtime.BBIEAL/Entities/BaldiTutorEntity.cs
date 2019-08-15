9c7fb365432cc7a0de3da8ac1ce6977ee0ae5d8f
DJMAN <aiden.hapless@gmail.com>
2019-08-15 14:57:44 -0600

    Create BaldiTutorEntity.cs

---
+++ BaldiModder.Runtime.BBIEAL/Entities/BaldiTutorEntity.cs
@@ -0,0 +1,18 @@
+﻿using BaldiModder.Data;
+using BaldiModder.Runtime.Entities;
+
+namespace BaldiModder.Runtime.BBIEAL.Entities {
+    [RunAlongSide("BaldiTutor")]
+    [EntityDefinition("BaldiTutor")]
+    public class BaldiTutorEntity : AnimatedEntity {
+
+        [ConfigurableEntityField] public string WaveAnimation { get; set; }
+
+        private void Start() {
+            if (OverrideAnimation) {
+                animationController.PlayAnimation(WaveAnimation, AnimationPlayStyle.GoToIdleState);
+            }
+        }
+
+    }
+}

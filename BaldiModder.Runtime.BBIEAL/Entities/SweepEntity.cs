addbbfd3defa0acd5c92717fc0e1e03b95c66088
DJMAN <aiden.hapless@gmail.com>
2019-08-15 18:34:36 -0600

    Create SweepEntity.cs

---
+++ BaldiModder.Runtime.BBIEAL/Entities/SweepEntity.cs
@@ -0,0 +1,27 @@
+﻿using BaldiModder.Runtime.Entities;
+
+namespace BaldiModder.Runtime.BBIEAL.Entities {
+    [RunAlongSide("Sweep")]
+    [EntityDefinition("Sweep")]
+    public class SweepEntity : AnimatedEntity {
+
+        [ConfigurableEntityField] public float TimeUntilActive { get; set; } = -1;
+
+        public float CoolDown { get => (float)GetFieldValue("CoolDown"); set => SetFieldValue("CoolDown", value); }
+        public float WaitTime { get => (float)GetFieldValue("WaitTime"); set => SetFieldValue("WaitTime", value); }
+
+        public float Wanders { get => (float)GetFieldValue("Wanders"); set => SetFieldValue("Wanders", value); }
+
+        public float Active { get => (float)GetFieldValue("Active"); set => SetFieldValue("Active", value); }
+
+        private bool overridenCooldown = false;
+
+        private void LateUpdate() {
+            if (!overridenCooldown && TimeUntilActive != -1) {
+                WaitTime = TimeUntilActive;
+                overridenCooldown = true;
+            }
+        }
+
+    }
+}

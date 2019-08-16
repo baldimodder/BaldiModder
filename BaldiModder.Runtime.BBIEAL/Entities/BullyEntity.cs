42e76a5847062140ca32a9b220f2115ed17fb456
DJMAN <aiden.hapless@gmail.com>
2019-08-15 19:10:33 -0600

    Create BullyEntity.cs

---
+++ BaldiModder.Runtime.BBIEAL/Entities/BullyEntity.cs
@@ -0,0 +1,16 @@
+﻿using BaldiModder.Runtime.Entities;
+
+namespace BaldiModder.Runtime.BBIEAL.Entities {
+    [RunAlongSide("Bully")]
+    [EntityDefinition("Bully")]
+    public class BullyEntity : AnimatedEntity {
+
+        public float ActiveTime { get => (float)GetFieldValue("ActiveTime"); set => SetFieldValue("ActiveTime", value); }
+        public float WaitTime { get => (float)GetFieldValue("WaitTime"); set => SetFieldValue("WaitTime", value); }
+        public float Guilt { get => (float)GetFieldValue("Guilt"); set => SetFieldValue("Guilt", value); }
+
+        public bool Active { get => (bool)GetFieldValue("Active"); }
+        public bool HasSpoken { get => (bool)GetFieldValue("HasSpoken"); }
+
+    }
+}

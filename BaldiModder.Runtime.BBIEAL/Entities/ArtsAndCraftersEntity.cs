e49c486b3c6925e7f7a48f27be7d98ce3f47df4a
DJMAN <aiden.hapless@gmail.com>
2019-08-16 16:29:27 -0600

    Create ArtsAndCraftersEntity.cs

---
+++ BaldiModder.Runtime.BBIEAL/Entities/ArtsAndCraftersEntity.cs
@@ -0,0 +1,19 @@
+﻿using BaldiModder.Data;
+using BaldiModder.Runtime.Entities;
+
+namespace BaldiModder.Runtime.BBIEAL.Entities {
+    [RunAlongSide("ArtsAndCrafters")]
+    [EntityDefinition("ArtsAndCrafters")]
+    public class ArtsAndCraftersEntity : AnimatedEntity {
+
+        public bool Angry { get => (bool)GetFieldValue("Angry"); set => SetFieldValue("Angry", value); }
+        public bool GettingAngry { get => (bool)GetFieldValue("GettingAngry"); set => SetFieldValue("GettingAngry", value); }
+
+        public float Anger { get => (float)GetFieldValue("Anger"); set => SetFieldValue("Anger", value); }
+
+        public float ForceShowTime { get => (float)GetFieldValue("ForceShowTime"); set => SetFieldValue("ForceShowTime", value); }
+
+        //TODO: Angry animation.
+
+    }
+}

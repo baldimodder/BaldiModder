0aaf3ccacee907a36ade18a14054fdb0f626e608
DJMAN <aiden.hapless@gmail.com>
2019-08-20 13:23:55 -0600

    TRIED to implement the BaldiFeed entity, but I ended up getting extremely frustrated and left it in it's broken state.

---
+++ BaldiModder.Runtime.BBIEAL/Entities/BaldiFeedEntity.cs
@@ -0,0 +1,28 @@
+﻿using BaldiModder.Data;
+using BaldiModder.Runtime.Entities;
+
+using UnityEngine;
+
+namespace BaldiModder.Runtime.BBIEAL.Entities {
+    [RunAlongSide("BaldiFeed")]
+    [EntityDefinition("BaldiFeed")]
+    public class BaldiFeedEntity : AnimatedEntity {
+
+        [ConfigurableEntityField] public string TalkAnimation { get; set; }
+        [ConfigurableEntityField] public string AngryAnimation { get; set; }
+
+        private bool angry = false;
+
+        private void Update() {
+            if (!OverrideAnimation) return;
+
+            //TODO: For some reason, the angry animation won't go past one or two frames. I'm too annoyed with this issue to fix it right now.
+            //TODO: Implement the talk animation.
+
+            if (animator.GetBool("angry")) {
+                animationController.PlayAnimation(AngryAnimation, AnimationPlayStyle.StayOnLastFrame, false);
+            }
+        }
+
+    }
+}
